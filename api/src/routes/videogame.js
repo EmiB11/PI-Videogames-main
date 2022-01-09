2c1fde7988bc5001518df015e63767bc5a97648e
Emi <emibarberis26@gmail.com>
2022-01-09 02:34:23 -0300

    ruta videogame fix

---
+++ api/src/routes/videogame.js
@@ -0,0 +1,89 @@
+require('dotenv').config();
+const { Router} = require('express');
+const { Videogame , Genre} = require('../db')
+const { MY_API_KEY} = process.env
+const axios = require('axios');
+
+const router = Router();
+
+router.post('/' ,async (req, res)=>{
+const {name , description , image , released , rating , platforms , genres , createdInDb}= req.body
+  try{
+    const createVideogame = await Videogame.create({
+        name,
+        description,
+        image,
+        released , 
+        rating , 
+        platforms ,
+       createdInDb
+    })
+
+    const genresDb = await Genre.findAll({
+        where:{name: genres}
+    })
+    createVideogame.addGenre(genresDb)
+    res.send('Videojuego creado con exito')
+    
+  }catch(e){
+    res.send('No se pudo crear el videojuego')
+  }
+
+})
+
+router.get('/:id' , (req , res)=>{
+    const {id}= req.params
+    if(id.length >= 36){
+        Videogame.findOne({
+            where:{id},
+            include:{model: Genre}
+        })
+        .then(data => res.json(data))
+        .catch(() => res.status(404).send('Videojuego no encontrado'))
+    }else{
+        axios.get(`https://api.rawg.io/api/games/${id}?key=${MY_API_KEY}`)
+        .then(res => res.data)
+        .then(data => {
+          return {
+          name: data.name,
+          id: data.id,
+          image: data.background_image,
+          description: data.description_raw,
+          released: data.released,
+          rating: data.rating,
+          platforms: data.platforms,
+          genres: data.genres
+          }
+        })
+        .then(response => res.json(response))
+        .catch(()=> res.status(404).send('Videojuego no encontrado'))
+    }   
+
+})
+
+router.delete('/:id' , (req , res)=>{
+    const {id} = req.params
+    Videogame.destroy({
+        where: {id}
+    })
+    .then(() => res.send('Videojuego eliminado'))
+    .catch((e)=> res.send(e))
+})
+
+router.put('/:id' , (req , res)=> {
+    const {id} = req.params;
+    const {name , image ,released, rating , platforms } = req.body
+    Videogame.findByPk(id).then(game => {
+        game.update({
+          name,
+          image,
+          released,
+          rating,
+          platforms
+        }).then((gameUpdate) => {
+          res.json(gameUpdate);
+        });
+      });
+})
+
+module.exports = router;
