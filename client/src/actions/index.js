c01af1c60fdff3413c4503920778abd2134bb726
Emi <emibarberis26@gmail.com>
2022-01-09 02:34:51 -0300

    actions

---
+++ client/src/actions/index.js
@@ -0,0 +1,69 @@
+import {GET_ALL_VIDEOGAMES , 
+        GET_BY_NAME , 
+        GET_DETAILS ,
+        DELETE_GAME ,
+       FILTER_BY ,ORDER_BY,
+       ORDER_GENRES,
+       ORDER_RATING,
+       GET_GENRES } from './types';
+
+import axios from 'axios';
+
+export const getAllVideogames = () => dispatch => {
+    return fetch('http://localhost:3001/videogames')
+            .then(res => res.json())
+            .then(data => dispatch({type:GET_ALL_VIDEOGAMES , payload: data}))
+}
+
+export const getVideogameByName = (name) => dispatch =>{
+    return fetch(`http://localhost:3001/videogames?name=${name}`)
+           .then(res => res.json())
+           .then(data => dispatch({type:GET_BY_NAME , payload: data}))
+}
+
+export const getDetails = (id) => dispatch => {
+    return fetch(`http://localhost:3001/videogame/${id}`)
+           .then(res => res.json())
+           .then(data => dispatch({type:GET_DETAILS , payload: data}))
+}
+
+export const deleteVideogame = id => dispatch => {
+    return axios.delete(`http://localhost:3001/videogame/${id}`)
+                .then(res => res.data)
+                .then(data => dispatch({type:DELETE_GAME , payload: data}))
+                .catch(err => console.log(err))
+}
+
+export const filterBy = (payload) => {
+    return {
+        type: FILTER_BY ,
+        payload
+    }
+}
+
+export const orderBy = payload => {
+    return {
+        type: ORDER_BY,
+        payload
+    }
+}
+export const orderRating = payload =>{
+    return {
+        type: ORDER_RATING,
+        payload
+    }
+}
+
+export const orderGenres = payload => {
+    return {
+        type: ORDER_GENRES,
+        payload
+    }
+}
+
+export const getGenres = () => dispatch => {
+
+    return fetch('http://localhost:3001/genres')
+           .then(res => res.json())
+           .then(data => dispatch({type:GET_GENRES , payload: data}))
+}
---
+++ client/src/actions/types.js
@@ -0,0 +1,9 @@
+export const GET_ALL_VIDEOGAMES = 'GET_ALL_VIDEOGAMES';
+export const GET_BY_NAME = 'GET_BY_NAME';
+export const GET_DETAILS = 'GET_DETAILS';
+export const DELETE_GAME = 'DELETE_GAME';
+export const FILTER_BY = 'FILTER_BY';
+export const ORDER_BY = 'ORDER_BY';
+export const ORDER_GENRES = 'ORDER_GENRES';
+export const ORDER_RATING = 'ORDER_RATING';
+export const GET_GENRES = 'GET_GENRES';
