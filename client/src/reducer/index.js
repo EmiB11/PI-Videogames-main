7b1ded4cb0ccdcb220d02affe568db25d9e32292
Emi <emibarberis26@gmail.com>
2022-01-17 16:36:09 -0300

    aplicando estilos y detalles

--- client/src/reducer/index.js
+++ client/src/reducer/index.js
@@ -2,13 +2,15 @@ import {GET_ALL_VIDEOGAMES ,
      GET_BY_NAME , GET_DETAILS , 
      DELETE_GAME , FILTER_BY,ORDER_BY,
      ORDER_GENRES,
-     ORDER_RATING,GET_GENRES} from '../actions/types';
+     ORDER_RATING,GET_GENRES , RESET_STATE } from '../actions/types';
 
 const initialState = {
     videogames : [],
     copyVideogames: [],
+    filtered: [],
     genres: [],
-    details: {}
+    details: {},
+   
 }
 
 export default function rootReducer(state = initialState , action){
@@ -18,13 +20,13 @@ export default function rootReducer(state = initialState , action){
             ...state ,
             videogames: action.payload,
             copyVideogames: action.payload,
-            
+            filtered: action.payload
         }
         case GET_BY_NAME:
              return {
             ...state,
-           videogames: action.payload,
-          
+          copyVideogames: action.payload,
+          filtered: action.payload
         }
         case GET_DETAILS: 
               return {
@@ -42,53 +44,63 @@ export default function rootReducer(state = initialState , action){
 
         }
         case FILTER_BY: 
-        const allGames = [...state.copyVideogames]
-        const filterGames = action.payload === 'default' ? state.copyVideogames : action.payload === 'created' ?
-                            allGames.filter(game => game.createdInDb) : allGames.filter(game => !game.createdInDb)
-          return {
-              ...state,
-              videogames: filterGames
-          }
+        if (action.payload === 'default'){
+            return {...state, filtered: [...state.copyVideogames]}
+            }
+          
+        if(action.payload === 'created'){
+            return {...state, filtered: state.copyVideogames.filter((game)=> (typeof game.id) === 'string')}
+            }
+          
+        if(action.payload === 'api'){
+            return {...state, filtered: state.copyVideogames.filter((game)=> (typeof game.id) === 'number')}
+            }
+            break
+         
         case ORDER_GENRES:
          const games = state.copyVideogames
-         const filterGenres = games.filter(g => g.genres.includes(action.payload))
+        const filterGenres =  games.filter(g => g.genres.includes(action.payload))
          return {
              ...state,
-             videogames: action.payload === 'todos' ? state.copyVideogames : filterGenres 
+             filtered: action.payload === 'todos' ? state.copyVideogames : filterGenres.length > 0  ? filterGenres  : 'Erro404' 
          }
          case ORDER_BY:
-             const sortGame = action.payload === 'A-Z' ? 
-             state.copyVideogames.sort((a,b)=>{
-                 if(a.name.toLowerCase() > b.name.toLowerCase()) return 1
-                 if(b.name.toLowerCase() > a.name.toLowerCase()) return -1
-                 return 0
-             }) 
-             :  state.copyVideogames.sort((a,b)=>{
-                if(a.name.toLowerCase() > b.name.toLowerCase()) return 1
-                if(b.name.toLowerCase() > a.name.toLowerCase()) return -1
-                return 0
-             })
-             return {
-                 ...state,
-                 videogames: sortGame
-             } 
+            if(action.payload === 'A-Z'){
+                return {...state, filtered: [...state.filtered].sort((a, b) => {
+                    if(a.name.toLowerCase() > b.name.toLowerCase()) return 1
+                    if(a.name.toLowerCase() < b.name.toLowerCase()) return -1
+                    return 0
+                })}}
+                  
+            if(action.payload === 'Z-A'){
+                return {...state, filtered: [...state.filtered].sort((a, b) => {
+                    if(a.name.toLowerCase() > b.name.toLowerCase()) return -1
+                    if(a.name.toLowerCase() < b.name.toLowerCase()) return 1
+                    return 0
+                })}}
+            else {
+                  return {...state, filtered: state.copyVideogames}
+
+                };
+
           case ORDER_RATING:
-              const sortRating = action.payload === 'asc' ?
-              state.videogames.sort((a,b)=> {
-                 
-                if(a.rating > b.rating) return 1
-                if(b.rating > a.rating) return -1
-                return 0
-              })
-              : state.videogames.sort((a,b)=>{
-                if(a.rating > b.rating) return 1
-                if(b.rating > a.rating) return -1
-                return 0
-              })
-              return {
-                  ...state,
-                  videogames: sortRating
-              }
+            if(action.payload === 'desc'){
+                return {...state, filtered: [...state.filtered].sort((a,b) => a.rating - b.rating)}
+               }
+                  
+            if(action.payload === 'asc'){
+                return {...state, filtered: [...state.filtered].sort((a,b) => b.rating - a.rating)}
+                }     
+            else {
+                return {...state, filtered: state.copyVideogames}
+                };
+
+           case RESET_STATE:
+               return {
+                   details: initialState.details
+               }
+            
+           
 
         default: return state;
     }
