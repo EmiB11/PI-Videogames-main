ec67327c7d18fd9c2893ce8fbba5b42fd856be81
Emi <emibarberis26@gmail.com>
2022-01-20 22:09:59 -0300

    deploym

--- client/src/index.js
+++ client/src/index.js
@@ -9,7 +9,7 @@ import dotenv from 'dotenv'
 import axios from 'axios';
 dotenv.config()
 
-axios.defaults.baseURL = process.env.REACT_API || "http://localhost:3001";
+axios.defaults.baseURL = process.env.REACT_API_KEY || "http://localhost:3001";
 
 ReactDOM.render(
   <Provider store = {store} >
